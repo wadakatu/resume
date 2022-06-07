2e14e6be1f1dc6b1393eee909ede579d3441bc9a
wadakatu <wadakatukoyo330@gmail.com>
2022-06-08 00:40:43 +0900

    add md-to-pdf package

---
+++ pdf/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stylesheet: "./pdf/style.css",
+  body_class: "markdown-body",
+  pdf_options: {
+    format: "A4",
+    margin: "30mm 20mm",
+    printBackground: true,
+    headerTemplate:
+      "<style> section {margin: 0 auto; font-family: system-ui; font-size: 11px;}</style>",
+    footerTemplate:
+      "<section><div><span class='pageNumber'></span> / <span class='totalPages'></span></div></section>",
+  },
+};
